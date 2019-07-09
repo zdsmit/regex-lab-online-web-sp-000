7ce3db5903b7c77e8a14416c59468612ca5edd15
Zach  Smith <zdsmith001@gmail.com>
2019-07-09 21:15:11 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
 def starts_with_a_vowel?(word)
-
+  if word = (/\b\w[aeiou]\b/)
+    true
+  elsif word = (/\b\w[^aeiou]\b/)
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
