c17b16e6dac87209b49fb7169575c2a87d3e7eb2
Zach  Smith <zdsmith001@gmail.com>
2019-07-10 15:08:11 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/\d{9}/) ? true : false
+  phone.match(/([0-9]{3})([0-9]{3})([0-9]{3})/) ? true : false
 end
