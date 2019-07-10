a691deb0a83842fdbb2d81346106a34d8c445c73
Zach  Smith <zdsmith001@gmail.com>
2019-07-10 14:23:06 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.match(/[^aeiouAEIOU]\w/) ? true : false
-  end
+  word.match(/[^aeiouAEIOU]\w/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
