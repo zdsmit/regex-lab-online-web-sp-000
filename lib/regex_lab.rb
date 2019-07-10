e692a772c86f6fbf4438850c432747aaed3cac12
Zach  Smith <zdsmith001@gmail.com>
2019-07-10 15:02:34 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/\b^[A-Z]*\W$\b/) ? true : false
+  text.match(/\b^[A-Z]*[.!?]$\b/) ? true : false
 end
 
 def valid_phone_number?(phone)
