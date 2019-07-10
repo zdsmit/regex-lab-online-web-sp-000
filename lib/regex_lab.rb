b4257eddc710710d8babc3c51939b6c3659b19fa
Zach  Smith <zdsmith001@gmail.com>
2019-07-10 14:54:45 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\bun/ /ing\b/)
+  text.scan(/\bun*ing\b/)
 end
 
 def words_five_letters_long(text)
