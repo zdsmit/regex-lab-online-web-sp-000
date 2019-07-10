6403f349771176ae184305a669bbf51bce60df96
Zach  Smith <zdsmith001@gmail.com>
2019-07-10 14:32:09 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  word.scan(/^un$ing\w3/)
+  word.scan(/^un$ing\w/)
 end
 
 def words_five_letters_long(text)
