809dd966afc0ed7083d3152fe5ef028a090ba287
Zach  Smith <zdsmith001@gmail.com>
2019-07-10 15:04:05 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/^[A-Z]*[.!?]$/) ? true : false
+  text.match(/^[A-Z].+[.!?]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
